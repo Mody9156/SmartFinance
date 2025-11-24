cf17c984969e74f62aafb97bad59dd2b92faf8b5
KEITA Modibo <Modykeita2@gmail.com>
2025-11-24 20:13:07 +0100

    initialisation de la class

--- SmartFinance/SmartFinance/ViewModel/HomeViewModel.swift
+++ SmartFinance/SmartFinance/ViewModel/HomeViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import Observation
+import SwiftUI
+import SwiftData
+
+@Observable
+class HomeViewModel{
+    var context: ModelContext
+    var items : [Item]
+    
+    init(context: ModelContext, items: [Item]) {
+        self.context = context
+        self.items = items
+    }
+    private func addItem() {
+            let newItem = Item(timestamp: Date())
+            context.insert(newItem)
+        
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+       
+            for index in offsets {
+                context.delete(items[index])
+            }
+        
+    }
+}
