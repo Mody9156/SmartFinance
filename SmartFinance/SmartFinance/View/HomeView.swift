796f3d7a106e7a10cadb2b3162e1b8a2b316d5cb
KEITA Modibo <Modykeita2@gmail.com>
2025-11-26 21:09:54 +0100

    mise en place du design

--- SmartFinance/SmartFinance/View/HomeView.swift
+++ SmartFinance/SmartFinance/View/HomeView.swift
@@ -10,13 +10,43 @@ import SwiftUICharts
 
 struct HomeView: View {
     var demoData: [Double] = [8,23,54,32,12,37,7,23,43]
-  
+    @State var name : String = "Jimmy"
+    @State var total : Int = 12_111
     var body: some View {
         
-        ScrollView {
-            ZStack {
-//                Text("SmartFinance")
-                LineChartView(data: demoData, title: "Title", legend: "Legendary") // legend is optional
+        ZStack{
+            LinearGradient(
+                colors: [Color.white,Color.green],
+                startPoint: .bottom,
+                endPoint: .top
+            )
+            .ignoresSafeArea()
+            
+            VStack {
+                Text("Bienvenue \(name)")
+                    .foregroundStyle(.white)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                Text("$\(total)")
+                    .foregroundStyle(.white)
+                    .font(.title2)
+                    .padding()
+                LineChartView(data: demoData, title: "Title", legend: "Legendary")
+                
+              
+                    
+                    Button(action:{
+                        
+                    },label:{
+                        Text("Explorer")
+                            .foregroundStyle(.white)
+                    })
+                    .padding()
+                    .background(.green)
+                    .clipShape(Capsule())
+                        
             }
         }
     }
