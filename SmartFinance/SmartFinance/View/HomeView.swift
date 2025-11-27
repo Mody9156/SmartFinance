4ff059ed2456efe7b115ac4d2e6047148c45810f
KEITA Modibo <Modykeita2@gmail.com>
2025-11-27 11:02:49 +0100

    nouvelle stuct

--- SmartFinance/SmartFinance/View/HomeView.swift
+++ SmartFinance/SmartFinance/View/HomeView.swift
@@ -15,17 +15,22 @@ struct HomeView: View {
         VStack(alignment: .leading){
             HStack {
                 Image(systemName: "person.crop.circle.fill")
+                    .resizable()
+                    .frame(width: 30,height: 30)
                     .foregroundStyle(Color("titleColor"))
                     .padding()
                 
                 Spacer()
                 
                 Image(systemName: "gearshape")
+                    .resizable()
+                    .frame(width: 30,height: 30)
                     .foregroundStyle(Color("titleColor"))
                     .padding()
             }
             Text("Tableau de bord")
                 .foregroundStyle(Color("titleColor"))
+                .font(.title)
                 .fontWeight(.bold)
                 .padding()
             
@@ -33,6 +38,19 @@ struct HomeView: View {
             
         }
         .padding()
+    }
+}
+
+
+
+struct CustomImageSystem:View {
+    var image : String
+    var body: some View {
+        Image(systemName: image)
+            .resizable()
+            .frame(width: 30,height: 30)
+            .foregroundStyle(Color("titleColor"))
+            .padding()
     }
 }
 
