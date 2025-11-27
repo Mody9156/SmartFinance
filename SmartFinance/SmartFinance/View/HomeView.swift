0892c2dfc8e674f39b47139192e5a0a1b6eaed8a
KEITA Modibo <Modykeita2@gmail.com>
2025-11-27 16:08:10 +0100

    mise en place d'une toggle et d'une variable

--- SmartFinance/SmartFinance/View/HomeView.swift
+++ SmartFinance/SmartFinance/View/HomeView.swift
@@ -10,103 +10,106 @@ import SwiftUICharts
 
 struct HomeView: View {
     @Bindable var homeViewModel : HomeViewModel
-    
+    @State var activeToggle: Bool = false
     var body: some View {
         
-        VStack(alignment: .leading){
-            HStack {
-                CustomImageSystem(image: "person.crop.circle.fill")
+        ScrollView {
+            VStack(alignment: .leading){
+                HStack {
+                    CustomImageSystem(image: "person.crop.circle.fill")
+                    
+                    Spacer()
+                    
+                    CustomImageSystem(image: "gearshape")
+                }
+                Text("Tableau de bord")
+                    .foregroundStyle(Color("titleColor"))
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                ZStack(alignment: .bottom) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [
+                                        Color("containerColor_left"),
+                                        Color("containerColor_right")
+                                    ],
+                                    startPoint: .leading,
+                                    endPoint: .trailing
+                                )
+                            )
+                            .frame(height: 200)
+                        
+                        VStack(alignment: .center) {
+                            Text("Montant utilisé")
+                                .foregroundStyle(Color("textColor"))
+                            
+                            Text("$\(homeViewModel.newBalance.rounded())")
+                                .font(.title)
+                                .foregroundStyle(.white)
+                            
+                            let updateDifference = homeViewModel.updateDifferenceWithLastMonth()
+                            let displayDifference = homeViewModel.displayDifference()
+                            
+                            Label{
+                                Text(displayDifference)
+                                    .foregroundStyle(Color("textColor"))
+                                
+                            } icon:{
+                                Image(systemName: updateDifference)
+                                    .foregroundStyle(.white)
+                            }
+                        }
+                    }
+                    
+                    ZStack {
+                        Rectangle()
+                            .frame(height: 50)
+                            .foregroundStyle(.green)
+                            .opacity(0.9)
+                            .clipShape(RoundedRectangleLetAndRight(radius: 12))
+                        
+                        Button(action: {
+                            
+                        }) {
+                            Label{
+                                Text("Nouvelle transaction")
+                                    .foregroundStyle(.white)
+                            }icon:{
+                                Image(systemName: "plus.circle.fill")
+                                    .resizable()
+                                    .frame(width: 25,height: 25)
+                                    .foregroundStyle(.white)
+                            }
+                        }
+                    }
+                }
+                
+                ZStack(alignment: .topTrailing){
+                    RoundedRectangle(cornerRadius: 12)
+                        .frame(height: 400)
+                        .foregroundStyle(.white)
+                        .shadow(radius: 12)
+                    
+                    LineView(data: [8,23,500,32,12,37,7,23,43], title: "Fiance", legend: "Totalité des dépenses")
+                        .padding()
+                    
+                    HStack {
+                        Toggle(activeToggle ? "Semaine" : "Mois", isOn: $activeToggle)
+                        
+                        //                        Text(activeToggle ? "Semaine" : "Mois")
+                    }
+                    .padding()
+                }
                 
                 Spacer()
                 
-                CustomImageSystem(image: "gearshape")
             }
-            Text("Tableau de bord")
-                .foregroundStyle(Color("titleColor"))
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding()
-            
-            ZStack(alignment: .bottom) {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 12)
-                        .foregroundStyle(
-                            LinearGradient(
-                                colors: [
-                                    Color("containerColor_left"),
-                                    Color("containerColor_right")
-                                ],
-                                startPoint: .leading,
-                                endPoint: .trailing
-                            )
-                        )
-                        .frame(height: 200)
-                    
-                    VStack(alignment: .center) {
-                        Text("Montant utilisé")
-                            .foregroundStyle(Color("textColor"))
-                        
-                        Text("$\(homeViewModel.newBalance.rounded())")
-                            .font(.title)
-                            .foregroundStyle(.white)
-                        
-                        let updateDifference = homeViewModel.updateDifferenceWithLastMonth()
-                        let displayDifference = homeViewModel.displayDifference()
-                        
-                        Label{
-                            Text(displayDifference)
-                                .foregroundStyle(Color("textColor"))
-                            
-                        } icon:{
-                            Image(systemName: updateDifference)
-                                .foregroundStyle(.white)
-                        }
-                    }
-                }
-                
-                ZStack {
-                    Rectangle()
-                        .frame(height: 50)
-                        .foregroundStyle(.green)
-                        .opacity(0.9)
-                        .clipShape(RoundedRectangleLetAndRight(radius: 12))
-                    
-                    Button(action: {
-                        
-                    }) {
-                        Label{
-                            Text("Nouvelle transaction")
-                                .foregroundStyle(.white)
-                        }icon:{
-                            Image(systemName: "plus.circle.fill")
-                                .resizable()
-                                .frame(width: 25,height: 25)
-                                .foregroundStyle(.white)
-                        }
-                    }
-                }
-            }
-            
-            ZStack {
-                RoundedRectangle(cornerRadius: 12)
-                    .frame(height: 300)
-                    .foregroundStyle(.white)
-                
-                HStack {
-                    LineView(data: [8,23,54,32,12,37,7,23,43], title: "Fiance", legend: "Full screen")
-                    
-                    Toggle(isOn: <#T##Binding<Bool>#>) {
-                        Text("here")
-                    }
-                }
-            }
-
-            Spacer()
-            
-            
-            
+            .padding()
         }
-        .padding()
     }
 }
 
