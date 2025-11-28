b1247dc3bf1831edfe01a89152a8afd3f7131ca3
KEITA Modibo <Modykeita2@gmail.com>
2025-11-28 18:10:56 +0100

    nouvelle picker

--- SmartFinance/SmartFinance/View/AddTransactionView.swift
+++ SmartFinance/SmartFinance/View/AddTransactionView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct AddTransactionView: View {
     @State var name: String = ""
     @State var amount: Double = 0.0
-    
+    @State var from: [String] = ["$","€"]
+    @State var selectElement = 1
+    @State var selectElement_2 = 1
     let formatter: NumberFormatter = {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
@@ -18,15 +20,40 @@ struct AddTransactionView: View {
     }()
     
     var body: some View {
-        Form {
-            Section(header:Text("Informations de l'achat")) {
-                TextField("Veuillez entrez le nom du de l'achat",
-                          text: $name)
-                TextField(
-                    "Veuillez entrez le montant de l'achat",
-                    value:$amount,
-                    formatter:formatter
-                )
+        NavigationStack {
+            Form {
+                Section(header:Text("Informations de l'achat")) {
+                    TextField("Veuillez entrez le nom du de l'achat",
+                              text: $name)
+                    TextField(
+                        "Veuillez entrez le montant de l'achat",
+                        value:$amount,
+                        formatter:formatter
+                    )
+                }
+                
+                Section(header:Text("Conversion")) {
+                  
+                        Picker(selection: $selectElement) {
+                            ForEach(0..<from.count,id: \.self){ from in
+                                Text(self.from[from])
+                            }
+                        } label: {
+                            Text("From")
+                        }
+                        .pickerStyle(.navigationLink)
+                        
+                    Picker(selection: $selectElement_2) {
+                        ForEach(0..<from.count,id: \.self){ from in
+                            Text(self.from[from])
+                        }
+                    } label: {
+                        Text("To")
+                    }
+                    .pickerStyle(.navigationLink)
+                    
+                    
+                }
             }
         }
     }
