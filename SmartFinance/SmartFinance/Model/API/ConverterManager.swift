13257df62ec53068259b731314717eaa2375a935
KEITA Modibo <Modykeita2@gmail.com>
2025-12-01 11:13:26 +0100

    nouvelle struct

--- SmartFinance/SmartFinance/Model/API/ConverterManager.swift
+++ SmartFinance/SmartFinance/Model/API/ConverterManager.swift
@@ -9,6 +9,15 @@ import Foundation
 
 class ConverterManager {
     
+    var session : HTTPService
+    
+    init(session: HTTPService = BasicHTTPClient()) {
+        self.session = session
+    }
+    
+    enum httpError : Error {
+        case invalidResponse
+    }
     
     func fetchData() -> URLRequest {
         let url = URL(string:"https://v6.exchangerate-api.com/v6/36bee9348b784097fe4c82d5/latest/USD")!
@@ -17,7 +26,17 @@ class ConverterManager {
         return request
     }
     
-    func showConverter() -> [Transaction] {
+    func showConverter() async throws -> [Transaction] {
+        let request = fetchData()
+        let (data,response) = try await session.fetchRequest(request)
         
+        guard let htppResponse = response as? HTTPURLResponse, htppResponse.statusCode == 200 else {
+            throw httpError.invalidResponse
+        }
+        
+        let decoder = JSONDecoder()
+        let result = try? decoder.decode([Transaction.self], from: data)
+        
+        return result
     }
 }
---
+++ SmartFinance/SmartFinance/Model/convert.swift
@@ -0,0 +1,12 @@
+//
+//  convert.swift
+//  SmartFinance
+//
+//  Created by Modibo on 01/12/2025.
+//
+
+import Foundation
+
+struct convert : Codable {
+    
+}
