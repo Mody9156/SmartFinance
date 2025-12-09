95b0a4e99a23f5af6d65ac40699cb08b9c54f29e
KEITA Modibo <Modykeita2@gmail.com>
2025-12-09 19:28:18 +0100

    initialiation de la class

--- SmartFinance/SmartFinance/ViewModel/UserProfileService.swift
+++ SmartFinance/SmartFinance/ViewModel/UserProfileService.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import Observation
+
+class UserProfileService {
+    var CurrencySymbols: [String : CurrencySymbol] = {
+        var dict: [String : CurrencySymbol] = [:]
+        CurrencySymbol.allCases.forEach { currency  in
+            dict[currency.rawValue] = currency
+        }
+        return dict
+    }()
+    
+    func selectedCurrencySymbolse(element: String) -> String {
+        return CurrencySymbols[element]?.symbol ?? "?"
+        
+    }
+    
+}
