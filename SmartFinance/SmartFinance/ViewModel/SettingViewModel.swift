d7d37f5d257393f64f6ede20cbcb758556ecb991
KEITA Modibo <Modykeita2@gmail.com>
2025-12-10 11:04:50 +0100

    nouvelle instance

--- SmartFinance/SmartFinance/ViewModel/SettingViewModel.swift
+++ SmartFinance/SmartFinance/ViewModel/SettingViewModel.swift
@@ -6,7 +6,17 @@
 //
 
 import Foundation
+import Observation
 
+@Observable
 class SettingViewModel {
+  let userProfile: UserProfileService
+    
+    init(userProfile: UserProfileService = UserProfileService()) {
+        self.userProfile = userProfile
+    }
+    
+    
+    
     
 }
